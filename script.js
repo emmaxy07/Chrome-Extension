c02c908947a8dfb00797705f1d3d4d3aebc85fb8
Emmanuel Omole <eomole@systemspecs.com.ng>
2022-01-01 12:46:00 +0100

    added the save tab and delete all buttons and their respective functionality

--- script.js
+++ script.js
@@ -2,28 +2,49 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById('tab-btn')
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 console.log(leadsFromLocalStorage)
 
-
-inputBtn.addEventListener("click", function(){
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage
   renderLeads()
-  console.log( localStorage.getItem("myLeads"))
+}
+
+tabBtn.addEventListener("click", function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setTab("myLeads", JSON.stringify( myLeads ))
+    render(myLeads)
+  }) 
 })
 
-function renderLeads(){
+function render(leads){
   let listItems = ""
-for(let i = 0; i < myLeads.length; i++){
+for(let i = 0; i < leads.length; i++){
   listItems += `
   <li>
-  <a target='_blank' href='${myLeads[i]}'> ${myLeads[i]} </a>
+  <a target='_blank' href='${leads[i]}'> ${myLeads[i]} </a>
   </li>
   `
 }
 
 ulEl.innerHTML += listItems
 }
+
+deleteBtn.addEventListener("dblclck", function(){
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+inputBtn.addEventListener("click", function(){
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+})
+
+
